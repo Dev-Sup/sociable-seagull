826ab92e4b219b0b1ed82ceb614a4334cc175350
Sergey Pedko <nitrotoll@gmail.com>
2017-02-08 17:37:37 +0200

    Add OAuth sender and simple handler

--- flaskapp.py
+++ flaskapp.py
@@ -22,9 +22,26 @@ def test():
 
 @app.route('/page')
 def page():
-    data = requests.get('http://devsup.pp.ua').content
+    url = 'https://oauth.vk.com/authorize'
+    client_id = '5861520'
+    scope = '1311746'
+    
+    payload = {
+        'client_id' : client_id,
+        'redirect_uri' : 'http://sociableseagull-devsup.rhcloud.com/oauth',
+        'display' : 'popup',
+        'scope' : scope,
+        'response_type' : 'code',
+        'v' : '5.62',
+    }
+    
+    data = requests.get(url, params = payload).content
     return data
     #return "It works!"
+    
+@app.route('/oauth')
+def oauth() :
+    return '1'
 
 if __name__ == '__main__':
     app.run()
